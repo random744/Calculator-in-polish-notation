21878d9b9d305e99add8478677a4051aafb38dbf
u72098 <ti-berger@outlook.com>
2024-04-15 11:36:35 +0200

    Endless loop added

--- calculator.sh
+++ calculator.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
- 
+
 calculate() {
- 
     num1=$1
     num2=$2
     operator=$3
- 
+
     if [ "$operator" = "+" ]; then
         echo $(($num1 + $num2))
     elif [ "$operator" = "-" ]; then
@@ -18,7 +17,15 @@ calculate() {
         echo "This is not a correct calculation!"
     fi
 }
- 
-read -p "What is your calculation? (e.g. 7 3 +, -, x, /): " operator num1 num2
-result=$(calculate $operator $num1 $num2)
-echo "Result: $result"
+
+while true; do
+    read -p "What is your calculation? (e.g. 7 3 +, -, x, / or 'q' to finish): " num1 num2 operator
+
+    if [[ $num1 = "q" || $num2 = "q" || $operator = "q" ]]; then
+        echo "Script is terminated..."
+        break
+    fi
+
+    result=$(calculate $num1 $num2 $operator)
+    echo "Result: $result"
+done
