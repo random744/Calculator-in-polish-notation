50853c6d6f296520c5b3c13c78d42394e7b037ae
u72098 <ti-berger@outlook.com>
2024-04-21 10:32:55 +0200

    fix issue errorhandling

--- calculator.sh
+++ calculator.sh
@@ -12,9 +12,13 @@ calculate() {
     elif [ "$operator" = "x" ]; then
         echo $(($num1 * $num2))
     elif [ "$operator" = "/" ]; then
-        echo $(($num1 / $num2))
+        if [ $num2 -eq 0 ]; then
+            echo "Error: Division by zero is not allowed"
+        else
+            echo "scale=2; $num1 / $num2" | bc
+        fi
     else
-        echo "This is not a correct calculation!"
+        echo "Error: Invalid operator '$operator'"
     fi
 }
 
@@ -22,10 +26,14 @@ while true; do
     read -p "What is your calculation? (e.g. 7 3 +, -, x, / or 'x' to finish): " num1 num2 operator
 
     if [[ $num1 = "x" || $num2 = "x" || $operator = "x" ]]; then
-        echo "Script is terminated..."
+        echo "Script terminated..."
         break
     fi
 
     result=$(calculate $num1 $num2 $operator)
-    echo "Result: $result"
+    if [[ $result = "Error:"* ]]; then
+        echo $result
+    else
+        echo "Result: $result"
+    fi
 done
