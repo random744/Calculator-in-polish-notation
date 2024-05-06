71bf5d6c1d992ac8ed79f8b49345f8428680fd4b
u72098 <ti-berger@outlook.com>
2024-05-06 13:37:15 +0200

    made more variants

--- calculator.sh
+++ calculator.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
-calculate() {
-    num1=$1
-    num2=$2
-    operator=$3
-
+ 
+num1=$1
+num2=$2
+operator=$3
+ 
+firstCalculationVariant() {
+ 
     if [ "$operator" = "+" ]; then
         echo $(($num1 + $num2))
     elif [ "$operator" = "-" ]; then
@@ -13,27 +14,106 @@ calculate() {
         echo $(($num1 * $num2))
     elif [ "$operator" = "/" ]; then
         if [ $num2 -eq 0 ]; then
-            echo "Error: Division by zero is not allowed"
+            echo "Error: Division by zero is not allowed!"
         else
-            echo "scale=2; $num1 / $num2" | bc
+            echo "scale=2; $num1 / $num2" | bc -l
         fi
     else
-        echo "Error: Invalid operator '$operator'"
+        echo "This is not a correct calculation!"
     fi
 }
-
+ 
+secondCalculationVariant() {
+ 
+    case $operator in
+            "+")        echo $(($num1 + $num2));;
+            "-")        echo $(($num1 - $num2));;
+            "x")        echo $(($num1 * $num2));;
+            "/")
+                        if [ $num2 -eq 0 ]; then
+                                echo "Error: Division by zero is not allowed!"
+                        else
+                                echo "scale=2; $num1 / $num2" | bc -l
+                        fi
+                        ;;
+            *)          echo "This is not a correct calculation!";;
+    esac
+}
+ 
+thirdCalculationVariant() {
+ 
+result=${allArguments[0]}
+ 
+if [ ${#allArguments[@]} -lt 3 ]; then  # das -lt steht für less than
+   echo "This is not a correct calculation!"
+   exit 1
+fi
+ 
+for (( i=1; i<${#allArguments[@]}-1; i++ ))
+do
+    if [ "${allArguments[${#allArguments[@]}-1]}" = "+" ]; then
+        result=$((result + allArguments[i]))
+ 
+    elif [ "${allArguments[${#allArguments[@]}-1]}" = "-" ]; then
+        result=$((result - allArguments[i]))
+ 
+    elif [ "${allArguments[${#allArguments[@]}-1]}" = "x" ]; then
+        result=$((result * allArguments[i]))
+ 
+    elif [ "${allArguments[${#allArguments[@]}-1]}" = "/" ]; then
+        if [ ${allArguments[i]} -eq 0 ]; then
+            echo "Error: Division by zero is not allowed!"
+        else
+            result=$(echo "scale=2; $result / ${allArguments[i]}" | bc -l)
+        fi
+    else
+            result="This is not a correct calculation!"
+    fi
+done
+ 
+echo "$result"
+}
+ 
+main1() {
+ 
 while true; do
-    read -p "What is your calculation? (e.g. 7 3 +, -, x, / or 'x' to finish): " num1 num2 operator
-
-    if [[ $num1 = "x" || $num2 = "x" || $operator = "x" ]]; then
-        echo "Script terminated..."
+ 
+    read -p "What is your calculation? (e.g. 7 3 +, -, x, / or 'q' to finish): " num1 num2 operator
+ 
+ 
+    if [[ $num1 = "q" || $num2 = "q" || $operator = "q" ]]; then
+        echo "Script is terminated..."
         break
     fi
-
-    result=$(calculate $num1 $num2 $operator)
+ 
+    result=$(firstCalculationVariant $num1 $num2 $operator)
+    #result=$(secondCalculationVariant $num1 $num2 $operator)
+ 
+    if [[ $result = "Error:"* ]]; then
+        echo $result
+    else
+        echo "Result: $result"
+    fi
+ 
+done
+}
+ 
+main2() {
+ 
+while true; do
+    read -p "What is your calculation? (e.g. 7 3 +, -, x, / or 'q' to finish): " -a allArguments
+ 
+    if [[ " ${allArguments[@]} " =~ " q " ]]; then
+        echo "Script is terminated..."
+        break
+    fi
+ 
+    result=$(thirdCalculationVariant "${allArguments[@]}")
+ 
     if [[ $result = "Error:"* ]]; then
         echo $result
     else
         echo "Result: $result"
     fi
 done
+}
